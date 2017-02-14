93fc1400fe650093abecf799faed1591130e29f6
Alex2508git config --global user.name Alex2508 <alexander_beschasnyy@mail.ru>
2017-02-14 19:49:29 +0200

    Initialize todo app

---
+++ app/components/Nav.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+    return(
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">React Todo App</li>
+                    <li>
+                        <IndexLink activeClassName="active-link" to="/">Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link activeClassName="active-link" to="/">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                    <ul className="menu">
+                        <li className="menu-text">Created by Alex Beschasnyy</li>
+                    </ul>
+            </div>
+        </div>
+    )
+}
+
+module.exports = Nav;
